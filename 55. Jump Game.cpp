f0bb86627fdb884c1bf2dfecc1c2da5cbcd7b1f7
Nguyễn Thị Thảo Lan <lan_2251220055@dau.edu.vn>
2026-05-12 00:28:30 +0700

    first commit

---
+++ 55. Jump Game.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int farthest = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > farthest) {
+                return false;
+            }
+
+            farthest = max(farthest, i + nums[i]);
+
+            if (farthest >= nums.size() - 1) {
+                return true;
+            }
+        }
+
+        return true;
+    }
+};
