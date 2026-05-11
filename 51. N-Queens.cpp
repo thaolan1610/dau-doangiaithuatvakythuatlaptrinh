1cc0fe12beaef1fcdd78c1b81c8b6b5adf1a9de9
Nguyễn Thị Thảo Lan <lan_2251220055@dau.edu.vn>
2026-05-12 00:16:44 +0700

    first commit

---
+++ 51. N-Queens.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> ans;
+    vector<string> board;
+    vector<bool> col, diag1, diag2;
+
+    void backtrack(int row, int n) {
+        if (row == n) {
+            ans.push_back(board);
+            return;
+        }
+
+        for (int c = 0; c < n; c++) {
+            if (col[c] || diag1[row - c + n - 1] || diag2[row + c]) {
+                continue;
+            }
+
+            board[row][c] = 'Q';
+            col[c] = true;
+            diag1[row - c + n - 1] = true;
+            diag2[row + c] = true;
+
+            backtrack(row + 1, n);
+
+            board[row][c] = '.';
+            col[c] = false;
+            diag1[row - c + n - 1] = false;
+            diag2[row + c] = false;
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        board = vector<string>(n, string(n, '.'));
+        col = vector<bool>(n, false);
+        diag1 = vector<bool>(2 * n - 1, false);
+        diag2 = vector<bool>(2 * n - 1, false);
+
+        backtrack(0, n);
+        return ans;
+    }
+};
