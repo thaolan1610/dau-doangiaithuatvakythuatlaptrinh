5d91430099dfdae4e7fb1e3093f3dc6e8b3321c3
Nguyễn Thị Thảo Lan <lan_2251220055@dau.edu.vn>
2026-05-12 00:24:59 +0700

    first commit

---
+++ 53. Maximum Subarray.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int cur = nums[0];
+        int ans = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            cur = max(nums[i], cur + nums[i]);
+            ans = max(ans, cur);
+        }
+
+        return ans;
+    }
+};
