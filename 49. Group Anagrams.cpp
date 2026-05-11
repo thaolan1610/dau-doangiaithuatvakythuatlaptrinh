d8df93180e852cd31a26058f64edb195045d6574
Nguyễn Thị Thảo Lan <lan_2251220055@dau.edu.vn>
2026-05-12 00:13:25 +0700

    first commit

---
+++ 49. Group Anagrams.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for (string s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());
+            mp[key].push_back(s);
+        }
+
+        vector<vector<string>> ans;
+        for (auto& it : mp) {
+            ans.push_back(it.second);
+        }
+
+        return ans;
+    }
+};
