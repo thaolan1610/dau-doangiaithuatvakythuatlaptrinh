e9f068216f1cb7645b6933e3df538a1feaea4a75
Nguyễn Thị Thảo Lan <lan_2251220055@dau.edu.vn>
2026-05-12 00:57:14 +0700

    first commit

---
+++ 78. Subsets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> path;
+
+    void backtrack(vector<int>& nums, int start) {
+        // mỗi trạng thái đều là 1 subset hợp lệ
+        res.push_back(path);
+
+        for (int i = start; i < nums.size(); i++) {
+            path.push_back(nums[i]);
+            backtrack(nums, i + 1);
+            path.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        backtrack(nums, 0);
+        return res;
+    }
+};
