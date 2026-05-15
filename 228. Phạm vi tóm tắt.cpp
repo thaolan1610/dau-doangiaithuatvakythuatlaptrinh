f6ad111aad701ec7ce541d73dff12458403768a1
Nguyễn Thị Thảo Lan <lan_2251220055@dau.edu.vn>
2026-05-15 13:19:04 +0700

    first commit

---
+++ 228. Phạm vi tóm tắt.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+
+        vector<string> result;
+
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+
+            int start = nums[i];
+
+            while (i + 1 < n && nums[i + 1] == nums[i] + 1) {
+                i++;
+            }
+
+            int end = nums[i];
+
+            if (start == end) {
+                result.push_back(to_string(start));
+            }
+            else {
+                result.push_back(
+                    to_string(start) + "->" + to_string(end)
+                );
+            }
+        }
+
+        return result;
+    }
+};
